4b9676cfd08cb9ff23a6b85fc85c570ddbce3569
Ted Summer <ted.summer2@gmail.com>
2019-01-24 18:39:08 -0600

    test(strelka): run bash file

--- strelka/strelkaTest.py
+++ strelka/strelkaTest.py
@@ -43,125 +43,126 @@ parsl.load(config)
 
 @bash_app
 def mysim(stdout=None, stderr=None):
-    cmd = """#!/usr/bin/env bash
+#     cmd = """#!/usr/bin/env bash
 
-echo "Hello World"
+# echo "Hello World"
 
-echo $STRELKA_INSTALL_PATH
+# echo $STRELKA_INSTALL_PATH
 
-#
-# Strelka - Small Variant Caller
-# Copyright (c) 2009-2018 Illumina, Inc.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# #
+# # Strelka - Small Variant Caller
+# # Copyright (c) 2009-2018 Illumina, Inc.
+# #
+# # This program is free software: you can redistribute it and/or modify
+# # it under the terms of the GNU General Public License as published by
+# # the Free Software Foundation, either version 3 of the License, or
+# # at your option) any later version.
+# #
+# # This program is distributed in the hope that it will be useful,
+# # but WITHOUT ANY WARRANTY; without even the implied warranty of
+# # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# # GNU General Public License for more details.
+# #
+# # You should have received a copy of the GNU General Public License
+# # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# #
+# #
 
-#
-# Execute small germline variant demonstration/verification run
-#
+# #
+# # Execute small germline variant demonstration/verification run
+# #
 
-set -o nounset
-set -o pipefail
+# set -o nounset
+# set -o pipefail
 
-scriptDir=$(dirname $0)
-demoDir=$scriptDir/../share/demo/strelka
-dataDir=$demoDir/data
-expectedDir=$demoDir/expectedResults
+# scriptDir=$(dirname $0)
+# demoDir=$scriptDir/../share/demo/strelka
+# dataDir=$demoDir/data
+# expectedDir=$demoDir/expectedResults
 
-analysisDir=./strelkaGermlineDemoAnalysis
+# analysisDir=./strelkaGermlineDemoAnalysis
 
-configScript=$scriptDir/configureStrelkaGermlineWorkflow.py
+# configScript=$scriptDir/configureStrelkaGermlineWorkflow.py
 
-demoConfigFile=$demoDir/strelkaGermlineDemoConfig.ini
-
-
-
-if [ ! -e $configScript ]; then
-    cat<<END 1>&2
-ERROR: Strelka must be installed prior to running demo.
-END
-    exit 2
-fi
+# demoConfigFile=$demoDir/strelkaGermlineDemoConfig.ini
 
 
-#
-# Step 1: configure demo
-#
-if [ -e $analysisDir ]; then
-    cat<<END 1>&2
-ERROR: Demo analysis directory already exists: '$analysisDir'
-       Please remove/move this to rerun demo.
-END
-    exit 2
-fi
 
-cmd="$configScript \
---bam='$dataDir/NA12891_demo20.bam' \
---bam='$dataDir/NA12892_demo20.bam' \
---referenceFasta='$dataDir/demo20.fa' \
---callMemMb=1024 \
---exome \
---disableSequenceErrorEstimation \
---runDir=$analysisDir"
-
-echo 1>&2
-echo "**** Starting demo configuration and run." 1>&2
-echo "**** Configuration cmd: '$cmd'" 1>&2
-echo 1>&2
-eval $cmd
-
-if [ $? -ne 0 ]; then
-    echo 1>&2
-    echo "ERROR: Demo configuration step failed" 1>&2
-    echo 1>&2
-    exit 1
-else
-    echo 1>&2
-    echo "**** Completed demo configuration." 1>&2
-    echo 1>&2
-fi
+# if [ ! -e $configScript ]; then
+#     cat<<END 1>&2
+# ERROR: Strelka must be installed prior to running demo.
+# END
+#     exit 2
+# fi
 
 
-#
-# Step 2: run demo (on single local core):
-#
-#stderrlog=$analysis_dir/make.stderr.log
-cmd="$analysisDir/runWorkflow.py -m local -j 1 -g 4"
-echo 1>&2
-echo "**** Starting demo workflow execution." 1>&2
-echo "**** Workflow cmd: '$cmd'" 1>&2
-echo 1>&2
-$cmd
+# #
+# # Step 1: configure demo
+# #
+# if [ -e $analysisDir ]; then
+#     cat<<END 1>&2
+# ERROR: Demo analysis directory already exists: '$analysisDir'
+#        Please remove/move this to rerun demo.
+# END
+#     exit 2
+# fi
+
+# cmd="$configScript \
+# --bam='$dataDir/NA12891_demo20.bam' \
+# --bam='$dataDir/NA12892_demo20.bam' \
+# --referenceFasta='$dataDir/demo20.fa' \
+# --callMemMb=1024 \
+# --exome \
+# --disableSequenceErrorEstimation \
+# --runDir=$analysisDir"
+
+# echo 1>&2
+# echo "**** Starting demo configuration and run." 1>&2
+# echo "**** Configuration cmd: '$cmd'" 1>&2
+# echo 1>&2
+# eval $cmd
+
+# if [ $? -ne 0 ]; then
+#     echo 1>&2
+#     echo "ERROR: Demo configuration step failed" 1>&2
+#     echo 1>&2
+#     exit 1
+# else
+#     echo 1>&2
+#     echo "**** Completed demo configuration." 1>&2
+#     echo 1>&2
+# fi
 
 
-if [ $? -ne 0 ]; then
-    cat<<END 1>&2
-ERROR: Workflow execution step failed
-END
-#        See make error log file: '$stderrlog'
-    exit 1
-else
-    echo 1>&2
-    echo "**** Completed demo workflow execution." 1>&2
-    echo 1>&2
-fi
+# #
+# # Step 2: run demo (on single local core):
+# #
+# #stderrlog=$analysis_dir/make.stderr.log
+# cmd="$analysisDir/runWorkflow.py -m local -j 1 -g 4"
+# echo 1>&2
+# echo "**** Starting demo workflow execution." 1>&2
+# echo "**** Workflow cmd: '$cmd'" 1>&2
+# echo 1>&2
+# $cmd
 
-echo 1>&2
-echo "**** Demo/verification successfully completed" 1>&2
-echo 1>&2
-"""
+
+# if [ $? -ne 0 ]; then
+#     cat<<END 1>&2
+# ERROR: Workflow execution step failed
+# END
+# #        See make error log file: '$stderrlog'
+#     exit 1
+# else
+#     echo 1>&2
+#     echo "**** Completed demo workflow execution." 1>&2
+#     echo 1>&2
+# fi
+
+# echo 1>&2
+# echo "**** Demo/verification successfully completed" 1>&2
+# echo 1>&2
+# """
+    cmd = 'echo "HEllo world!"'
     return cmd
 
 # mysim(inputs=[os.path.join(os.getcwd(), "bashFile.sh")]).result()

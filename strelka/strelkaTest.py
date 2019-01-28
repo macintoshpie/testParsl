b4f4aad193f79edb48a1efdb55ca0543bfd8f6af
Ted Summer <ted.summer2@gmail.com>
2019-01-28 13:45:28 -0600

    refactor(strelkaTest): add template formatting and conditional config

--- strelka/strelkaTest.py
+++ strelka/strelkaTest.py
@@ -1,4 +1,6 @@
 import os
+import sys
+from string import Template
 
 import parsl
 
@@ -7,10 +9,12 @@ from parsl.providers import AWSProvider
 from parsl.config import Config
 from parsl.executors.ipp import IPyParallelExecutor
 from parsl.executors.ipp_controller import Controller
+from parsl.executors.threads import ThreadPoolExecutor
+
 
 from parsl.app.app import python_app, bash_app
 
-config = Config(
+awsConfig = Config(
     executors=[
         IPyParallelExecutor(
             label='ec2_single_node',
@@ -31,47 +35,18 @@ config = Config(
     ],
 )
 
+localConfig = Config(
+    executors=[
+        ThreadPoolExecutor(
+            max_threads=8,
+            label='local_threads'
+        )
+    ]
+)
+
 parsl.set_stream_logger()
-parsl.load(config)
-
-@python_app
-def timeApp(cmd):
-    with open("/home/ubuntu/timeAppScript.sh", "w") as text_file:
-        text_file.write(cmd)
-
-    import subprocess
-    import time
-    t1 = time.time()
-    proc = subprocess.Popen(['bash', '/home/ubuntu/timeAppScript.sh'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    outs, errs = proc.communicate()
-    t2 = time.time()
-    return (outs, t2 - t1)
 
 cmd = """#!/usr/bin/env bash
-
-echo "Hello World"
-
-echo $STRELKA_INSTALL_PATH
-
-#
-# Strelka - Small Variant Caller
-# Copyright (c) 2009-2018 Illumina, Inc.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-
 #
 # Execute small germline variant demonstration/verification run
 #
@@ -85,13 +60,11 @@ dataDir=$demoDir/data
 expectedDir=$demoDir/expectedResults
 
 analysisDir=./strelkaGermlineDemoAnalysis
-
 configScript=$scriptDir/configureStrelkaGermlineWorkflow.py
-
 demoConfigFile=$demoDir/strelkaGermlineDemoConfig.ini
 
 
-
+# verify paths are correctly configured
 if [ ! -e $configScript ]; then
     cat<<END 1>&2
 ERROR: Strelka must be installed prior to running demo.
@@ -99,10 +72,6 @@ END
     exit 2
 fi
 
-
-#
-# Step 1: configure demo
-#
 if [ -e $analysisDir ]; then
     cat<<END 1>&2
 ERROR: Demo analysis directory already exists: '$analysisDir'
@@ -111,6 +80,7 @@ END
     exit 2
 fi
 
+# create workflow
 cmd="$configScript \
 --bam='$dataDir/NA12891_demo20.bam' \
 --bam='$dataDir/NA12892_demo20.bam' \
@@ -120,33 +90,10 @@ cmd="$configScript \
 --disableSequenceErrorEstimation \
 --runDir=$analysisDir"
 
-echo 1>&2
-echo "**** Starting demo configuration and run." 1>&2
-echo "**** Configuration cmd: '$cmd'" 1>&2
-echo 1>&2
 eval $cmd
 
-if [ $? -ne 0 ]; then
-    echo 1>&2
-    echo "ERROR: Demo configuration step failed" 1>&2
-    echo 1>&2
-    exit 1
-else
-    echo 1>&2
-    echo "**** Completed demo configuration." 1>&2
-    echo 1>&2
-fi
-
-
-#
-# Step 2: run demo (on single local core):
-#
-#stderrlog=$analysis_dir/make.stderr.log
-cmd="$analysisDir/runWorkflow.py -m local -j 1 -g 4"
-echo 1>&2
-echo "**** Starting demo workflow execution." 1>&2
-echo "**** Workflow cmd: '$cmd'" 1>&2
-echo 1>&2
+# run workflow
+cmd="$analysisDir/runWorkflow.py -m local -j ${nCPUs} -g ${memPerCPU}"
 $cmd
 
 
@@ -154,18 +101,39 @@ if [ $? -ne 0 ]; then
     cat<<END 1>&2
 ERROR: Workflow execution step failed
 END
-#        See make error log file: '$stderrlog'
     exit 1
-else
-    echo 1>&2
-    echo "**** Completed demo workflow execution." 1>&2
-    echo 1>&2
 fi
-
-echo 1>&2
-echo "**** Demo/verification successfully completed" 1>&2
-echo 1>&2
 """
 
-x = timeApp(cmd)
+if sys.platform == "darwin":
+    parsl.load(localConfig)
+    cmd = """#!/usr/bin/env bash
+bashVar="This is a bash var"
+echo "Hello World - ${bashVar}"
+echo "nCPUs: ${nCPUs}"
+echo "memPerCPU: ${memPerCPU}"
+"""
+else:
+    parsl.load(awsConfig)
+
+@python_app
+def timeApp(cmd, parameters):
+    scriptPath = os.path.expanduser("~/timeAppScript.sh")
+    cmd = Template(cmd).safe_substitute(parameters)
+    with open(scriptPath, "w") as text_file:
+        text_file.write(cmd)
+
+    import subprocess
+    import time
+    t1 = time.time()
+    proc = subprocess.Popen(['bash', scriptPath], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    outs, errs = proc.communicate()
+    t2 = time.time()
+    return (outs, t2 - t1)
+
+params = {
+    'nCPUs': 1,
+    'memPerCPU': 4
+}
+x = timeApp(cmd, params)
 print(x.result())

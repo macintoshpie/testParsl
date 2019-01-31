987fd0d59acdbee32174e15b8deebb38c3c51d23
Ted Summer <ted.summer2@gmail.com>
2019-01-30 18:31:22 -0600

    feat(nscc): init tests with nscc

---
+++ nscc/simpleTest.py
@@ -0,0 +1,53 @@
+import parsl
+
+from parsl.providers import AWSProvider
+
+from parsl.config import Config
+from parsl.executors.ipp import IPyParallelExecutor
+from parsl.executors.ipp_controller import Controller
+
+from parsl.app.app import python_app, bash_app
+
+initCmd = """#!/usr/bin/env bash
+module load anaconda/3
+conda create --name parsl_py36 python=3.6
+source activate parsl_py36
+
+# install parsl
+python3 -m pip install parsl
+"""
+
+userHome = '/home/users/industry/uchicago/tsummer2'
+
+config = Config(
+  executors=[
+    IPyParallelExecutor(
+      label='nscc_exec',
+      workers_per_node=1,
+      provider=TorqueProvider(
+        channel=LocalChannel(),
+        nodes_per_block=1,
+        init_blocks=2,
+        max_blocks=1,
+        launcher=AprunLauncher(),
+        scheduler_options='-P 11001079 -l mem=1G',
+        worker_init='',
+        walltime="00:5:00"
+      ),
+      controller=Controller(public_ip='192.168.25.4'),    # Please replace PUBLIC_IP with your public ip
+    )
+  ],
+)
+
+parsl.set_stream_logger()
+parsl.load(config)
+
+@python_app
+def mysim():
+    from random import randint
+    """Generate a random integer and return it"""
+    import time
+    time.sleep(10)
+    return randint(1,100)
+
+print(mysim().result())

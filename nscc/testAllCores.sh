76502b0b1a75acf930e9165feecd5fc3074dc384
Ted Summer <ted.summer2@gmail.com>
2019-02-09 18:45:22 -0600

    feat(nscc): test script for running strelka on 24 cores

--- nscc/testAllCores.sh
+++ nscc/testAllCores.sh
@@ -2,7 +2,7 @@
 
 #PBS -q normal
 #PBS -P 11001079
-#PBS -l nodes=1:ppn=24:mem=96G
+#PBS -l nodes=1:ppn=24
 #PBS -l walltime=00:20:00
 #PBS -N Strelka_Head
 #PBS -o /home/users/industry/uchicago/tsummer2/outputfiles/Strelka_Head.o
@@ -12,7 +12,7 @@ echo "Started head task"
 echo "Running in $(pwd)"
 
 ${STRELKA_INSTALL_PATH}/bin/configureStrelkaGermlineWorkflow.py \
-  --ref /home/projects/11001079/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna \
+  --ref /home/projects/11001079/references/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna \
   --bam /home/projects/11001079/giab/NHGRI_Illumina300X_Chinesetrio_novoalign_bams/chr21.bam \
   --runDir analysis
 

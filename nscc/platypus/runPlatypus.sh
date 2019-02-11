0c067d576917e699cfe3d5c8e7e27770aef2a4ee
Ted Summer <ted.summer2@gmail.com>
2019-02-11 17:32:15 -0600

    refactor(paropt): cleanup, also use json for config

--- nscc/platypus/runPlatypus.sh
+++ nscc/platypus/runPlatypus.sh
@@ -1,5 +1,5 @@
-bam="/home/projects/11001079/ms_data/chr21/bam/chr21rg.bam"
-ref="/home/projects/11001079/ms_data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna"
+bam="/home/projects/11001079/ts_data/chr21rg.bam"
+ref="/home/projects/11001079/ts_data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna"
 
 vcf="${HOME}/results/chr21platypus.vcf.gz"
 

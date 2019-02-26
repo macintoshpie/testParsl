23c311269aacc908f6150cf1228671a0fac8e021
Ted Summer <ted.summer2@gmail.com>
2019-02-26 14:14:12 -0600

    feat(platypus): add chr1 config

---
+++ nscc/platypus/runPlatypusChr1.sh
@@ -0,0 +1,12 @@
+bam="/home/projects/11001079/ts_data/chr1rg.bam"
+ref="/home/projects/11001079/ts_data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna"
+
+vcf="${HOME}/results/chr1platypus.vcf.gz"
+
+# see here for configs:
+# https://www.well.ox.ac.uk/research/research-groups/lunter-group/softwares/platypus-documentation
+
+echo "Loading platypus env"
+source activate platypus_py27
+echo "Starting platypus..."
+platypus callVariants --bamFiles=${bam} --refFile=${ref} --output=${vcf} --nCPU ${nCPU} --bufferSize ${bufferSize}
